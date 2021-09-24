40276018a49e317acc7ea7af7993d8a498b19376
Mayur Bhor <mayurtrap@gmail.com>
2021-09-24 17:59:40 +0530

    Added startingerror page routes

---
+++ Student/startingerrorpage_get.js
@@ -0,0 +1,41 @@
+1) http://localhost:10027/api/groups/{Group_id}
+
+E.g 1) http://localhost:10027/api/groups/5
+
+[
+    [
+        {
+            "Group_id": 5,
+            "Group_Name": "Btech-Computer Engineering-PRJ-21-22-1",
+            "startDate": "2021-07-13T18:30:00.000Z",
+            "Guidename": "Vikas Kolekar"
+        }
+    ],
+    [
+        {
+            "leadername": "Ezekiel Huber",
+            "leadermobile": 2056724768,
+            "leaderemail": "pyziwyta@mailinator.com"
+        }
+    ]
+]
+
+E.g 2) http://localhost:10027/api/groups/5
+
+[
+    [
+        {
+            "Group_id": 6,
+            "Group_Name": "Btech-Computer Engineering-PRJ-21-22-2",
+            "startDate": null,
+            "Guidename": "Vikas Kolekar"
+        }
+    ],
+    [
+        {
+            "leadername": "Macaulay Johnson",
+            "leadermobile": 8551749828,
+            "leaderemail": "julanudiby@mailinator.com"
+        }
+    ]
+]
